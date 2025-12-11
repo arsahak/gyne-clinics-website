f1aa6be9db0efbb14c35958db5fe908f521031f4
arsahak <arsahak99@gmail.com>
2025-12-11 22:34:13 +0600

    update og image and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
 import HeroSection from "@/component/home/HeroSection";
 import ServicesSection from "@/component/home/ServicesSection";
 
+export const metadata = {
+  title: "GyneClinics – Expertise Professionalism and Excellence",
+  description:
+    "We uphold the highest standards of medical ethics, ensuring total confidentiality, safety, and respect for every patient.",
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-USA",
+    },
+  },
+  openGraph: {
+    images: "/opengraph-image.jpg",
+  },
+};
+
 export default function Home() {
   return (
     <div className="">
