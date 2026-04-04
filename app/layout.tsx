a0e353a2f7debc5d32f99d7ba038e85fef05f6b5
arsahak <arsahak99@gmail.com>
2026-04-04 18:28:44 +0600

    udpate og url:

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  metadataBase: new URL("https://gyne-clinics-website.vercel.app"),
+  metadataBase: new URL("https://www.gyneclinics.com"),
 };
 
 export default function RootLayout({
